fad9f9ec197035432e695d50e08698a5e700c297
dmodoomsirius <dmodoomsirius@gmail.com>
2015-01-23 14:33:16 -0500

    Fix a mistake in dev.bat

--- dev.bat
+++ dev.bat
@@ -3,11 +3,11 @@ cd ..
 
 SET /P ANSWER=Would you use idea? (Y/N)? 
 echo You chose: %ANSWER% 
-if /i {%ANSWER%}=={idea} (goto :yes) 
-goto :idea 
+if /i {%ANSWER%}=={idea} (goto :idea) 
+goto :eclipse
 
 :idea
 gradlew setupdecompworkspace IDEA genIntellijRuns -PbambooshortPlanName=VoltzEngine -Pdmodcurse=61545 --refresh-dependencies
 
 :eclipse
-gradle setupdecompworkspace eclipse -PbambooshortPlanName=VoltzEngine --refresh-dependencies 
+gradle setupdecompworkspace cleanEclipse eclipse -PbambooshortPlanName=VoltzEngine --refresh-dependencies
